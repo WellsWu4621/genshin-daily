d6bb89b6216c022b54c557cc3a090b7f182eedc8
Wells Wu <wells.wu4621@gmail.com>
2022-02-03 21:29:34 -0800

    thinking of a standalone version

---
+++ standalone/app.js
@@ -0,0 +1,12 @@
+fetch(`https://hk4e-api-os.mihoyo.com/event/sol/sign?lang=en-us`,{
+  method: 'POST',
+  headers: {'Content-Type': 'application/json',
+  'Cookie': process.env.GENSHIN_COOKIE,
+  'User-Agent': process.env.GENSHIN_USER_AGENT,
+  "Origin": "https://webstatic-sea.mihoyo.com",
+  "Referer" : "https://webstatic-sea.mihoyo.com/"
+},
+  body: JSON.stringify({"act_id": process.env.GENSHIN_ACT_ID})
+}).then(res => {
+  console.log('Request Complete! response: ', res)
+});
